a8c249db483e6270b93772911e1eec2036b205d6
BorBoris <integran2003@yandex.ru>
2020-10-22 11:47:16 +0300

    Update cats.php

--- php/cats.php
+++ php/cats.php
@@ -8,36 +8,35 @@ class Cat {
     }
 
     function fight($anotherCat) {
-        
-        global $result;
-        
-            $ageMin = ($this->age = $anotherCat->age ? 1 : 0);
-            $weightMin = ($this->weight = $anotherCat->weight ? 1 : 0);
-            $strengthMin = ($this->strength = $anotherCat->strength ? 1 : 0);
+
+            $ageMin = ($this->age == $anotherCat->age ? 1 : 0);
+            $weightMin = ($this->weight == $anotherCat->weight ? 1 : 0);
+            $strengthMin = ($this->strength == $anotherCat->strength ? 1 : 0);
             $countMin = $ageMin + $weightMin + $strengthMin;
+            
 
             $agePlus = ($this->age > $anotherCat->age ? 1 : 0);
             $weightPlus = ($this->weight > $anotherCat->weight ? 1 : 0);
             $strengthPlus = ($this->strength > $anotherCat->strength ? 1 : 0);
             $count = $agePlus + $weightPlus + $strengthPlus;
-            $res = ($count>=2 ? 1 : 0);
             
-            if ($countMin = 3){
-                $result = "Paritet!";
-            }else {$result = ($res ? "Cat1 win!":"Cat2 win!");}
-        
+            if ($countMin == 3){
+                $result = "C1 true, C2 false!";
+            }else {$result = ($count>=2 ? ($this->name . "won!") :$this->name . "lost!");}
+            return $result;
     }
 }
 
 $Cat1 = new Cat();
+$Cat1->name = "C1";
 $Cat1->age=3;
 $Cat1->weight=6;
 $Cat1->strength=10;
 $Cat2 = new Cat();
-$Cat2->age=3;
-$Cat2->weight=6;
+$Cat2->name="C2"; 
+$Cat2->age=4;
+$Cat2->weight=7;
 $Cat2->strength=10;
-$Cat1->fight($Cat2);
-$ren = $Cat1->fight($Cat2);
+$result=$Cat2->fight($Cat1);
 echo $result;
 ?>
